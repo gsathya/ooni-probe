a43c7761d311acf59eeb7fff6d5e836a1c788885
Sathyanarayanan Gunasekaran <gsathya.ceg@gmail.com>
2012-03-21 17:29:02 +0530

    Add invalid_req and cached_obj tests

--- tests/squid.py
+++ tests/squid.py
@@ -12,6 +12,11 @@ from plugoo import gen_headers
 from plugoo.assets import Asset
 from plugoo.tests import Test
 
+try:
+    from BeautifulSoup import BeautifulSoup
+except:
+    pass
+
 __plugoo__ = "SquidProxy"
 __desc__ = "This Test aims at detecting the squid transparent proxy"
 
@@ -51,8 +56,6 @@ class Squid(Test):
             conn.putheaders(h[0], h[1])
         conn.endheaders()
 
-        send_browser_headers(self, None, conn)
-
         response = conn.getresponse()
 
         headers = dict(response.getheaders())
@@ -61,21 +64,54 @@ class Squid(Test):
         self.data = response.read()
         return True
 
-    def invalid_request(self):
+
+    def random_bad_request(self, url):
+        r_str = ''.join(random.choice(string.ascii_uppercase + string.digits) for x in range(random.randint(5,20)))
+        if self._http_request(self, r_str, url):
+            return True
+        else:
+            return None
+
+    def invalid_request(self, url):
         """
         This will trigger squids "Invalid Request" error.
         """
-        pass
 
-    def cache_object(self):
+        test_name = "squid bad request"
+        self.logger.info("RUNNING %s test" % test_name)
+        if self.random_bad_request(url):
+            s_headers = {'X-Squid-Error' : 'ERR_INVALID_REQ 0'}
+            for i in s_headers.items():
+                if i[0] in self.headers:
+                    self.logger.info("the %s test returned False" % test_name)
+                    return False
+            self.logger.info("the %s test returned True" % test_name)
+            return True
+        else:
+            self.logger.warning("the %s test returned failed" % test_name)
+            return None
+
+
+    def cache_object(self, url):
         """
         This attempts to do a GET cache_object://localhost/info on
         any destination and checks to see if the response contains
         is that of Squid.
         """
-
-        pass
-
+        test_name = "squid cacheobject"
+        self.logger.info("RUNNING %s test" % test_name)
+        if self._http_request(self, "GET", url, "cache_object://localhost/info"):
+            soup = BeautifulSoup(self.data)
+            if soup.find('strong') and soup.find('strong').string == "Access Denied.":
+                self.logger.info("the %s test returned False" % test_name)
+                return False
+            else:
+                self.logger.info("the %s test returned True" % test_name) 
+                return True
+        else:
+            self.logger.warning("the %s test failed" % test_name)
+            return None
+    
     def experiment(self, *a, **kw):
         """
         Fill this up with the tasks that should be performed
